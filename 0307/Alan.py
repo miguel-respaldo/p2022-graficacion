b4008d7f72929d869a69390af3ad977968ca5430
Alan Luna <za18011658@zapopan.tecmm.edu.mx>
2022-03-07 16:32:18 -0600

    Alan Luna

---
+++ 0307/Alan.py
@@ -0,0 +1,8 @@
+#Números:
+numero = eval(input("Escribe un número: "))
+print("Escribiste el número:", numero)
+
+#Texto:
+texto = input("Escribiste un texto: ")
+print("Escribiste el texto: " + texto)
+

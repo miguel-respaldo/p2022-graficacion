c3d77124f6b79be707450c3cb5d31b8260bcda9f
sebastianquijas <za18011215@zapopan.tecmm.edu.mx>
2022-03-08 18:17:06 -0600

    Operadores a nivel de bits

--- 0308/Sebastian.py
+++ 0308/Sebastian.py
@@ -1,6 +1,6 @@
 x = 22
 
-print ("x = {:>6b})".format (x))
+print ("x = {:>6b})".format  (x))
 print ("x & 4 = {: >3d} = {: >6b}". format ( x & 4 , x & 4) )
 print ("x | 1 = {: >3d} = {: >6b}". format ( x | 1 , x | 1) )
 print ("x ^ 4 = {: >3d} = {: >6b}". format ( x ^ 4 , x ^ 4) )

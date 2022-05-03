e2619fb79f9a2b61003e04a333fc6e2e8ca08a78
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-05-03 17:36:57 -0500

    camara

---
+++ 0503/03-blanco_y_negro.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+if not camara.isOpened():
+    print("No puedo abrir la camara.")
+    exit(1)
+
+while True:
+    # Leeo la imagen de la camara
+    retorno, imagen = camara.read()
+
+    if not retorno:
+        print("No puedo capturar la imagen de la camara")
+        break
+
+    grises = cv.cvtColor(imagen, cv.COLOR_BGR2GRAY)
+
+    cv.imshow("Camara", grises)
+
+    # Salgo del programa oprimiendo la tecla ESC
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()

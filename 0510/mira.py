a9de66654b3ab6f21030132e3ec7f0c33416dcef
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2022-05-10 19:15:16 -0500

    add mira

---
+++ 0510/mira.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+detector = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+if not camara.isOpened():
+    print("No puedo abrir la camara.")
+    exit(1)
+
+while True:
+    # Leeo la imagen de la camara
+    retorno, imagen = camara.read()
+
+    if not retorno:
+        print("No puedo capturar la imagen de la camara")
+        break
+
+    grises = cv.cvtColor(imagen, cv.COLOR_BGR2GRAY)
+    caras  = detector.detectMultiScale(grises, 1.1, 4)
+
+    for (x,y, w,h) in caras:
+        cv.rectangle(imagen, (x,y), (x+w,y+h), (255,0,0), 2)
+
+    cv.imshow("Camara", imagen)
+
+    # Salgo del programa oprimiendo la tecla ESC
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()
